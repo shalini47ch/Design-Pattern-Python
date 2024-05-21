9a083420fcdabf21a785f572ac7c2e5b37e4c5c5
shalini47ch <shalini47choudhary@gmail.com>
2024-05-21 20:30:40 +0530

    online store added

---
+++ StructuralDesignPattern/Facade/example3.py
@@ -0,0 +1,77 @@
+#Online store having subsystems like Product,Inventory,Payment and the facade is Online Store
+
+class Product:
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+    
+    def get_name(self):
+        return self.name
+    
+    def get_price(self):
+        return self.price
+
+#similarly create an inventory
+class Inventory:
+    def __init__(self):
+        self.products=[]
+    
+    def add_product(self,product):
+        self.products.append(product)
+    
+    def remove_product(self,product):
+        self.products.remove(product)
+    def get_product(self):
+        return self.products
+    
+class Payment:
+    def __init__(self):
+        self.total=0
+    
+    def add_product(self,product):
+        self.total+=product.get_price()
+    
+    def remove_product(self,product):
+        self.total-=product.get_price()
+        
+    def get_total(self):
+        return self.total
+
+
+#this is the facade which will be the single point with all the functionality in one
+class OnlineStore:
+    def __init__(self):
+        self.inventory=Inventory()
+        self.payment=Payment()
+    
+    def add_product_tocart(self,name,price):
+        product=Product(name,price)
+        self.inventory.add_product(product)
+        #similarly add it to the patment as well
+        self.payment.add_product(product)
+    
+    def remove_product_fromcart(self,name):
+        products=self.inventory.get_total()
+        for product in products:
+            if self.product.get_name()==name:
+                self.inventory.remove(product)
+                self.payment.remove(product)
+                break
+    
+    def checkout(self):
+        total=self.payment.get_total()
+        print(f"The total amount is {total}")
+
+#client
+if __name__=="__main__":
+    store=OnlineStore()
+    store.add_product_tocart("Shirt",20)
+    store.add_product_tocart("Trousers",40)
+    store.add_product_tocart("Sneekers",80)
+    store.checkout()
+    
+    
+    
+    
+    
+        
